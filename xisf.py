de2df6fa945f62d002f6844723778c105836f58a
Sergio <sdiaz@us.es>
2021-11-15 18:28:21 +0100

    Doc in markdown (using pydoc-markdown)

--- xisf.py
+++ xisf.py
@@ -34,30 +34,31 @@ class XISF:
     (using the "channels-last" convention by default). 
 
     What's supported: 
-     - Monolithic XISF files only
-       - XISF blocks with attachment block locations (neither inline nor embedded block locations as required 
-       for a complete baseline decoder)
-       - Planar pixel storage models, *however it assumes 2D images only* (with multiple channels)
-       - UInt8/16/32 and Float32/64 pixel sample formats
-       - Grayscale and RGB color spaces     
-     - Decoding:
-       - multiple Image core elements from a monolithic XISF file
-       - Support all standard compression codecs defined in this specification for decompression (zlib/lz4[hc]+
-       byte shuffling)
-     - Encoding:
-       - Single image core element
-       - Uncompressed data blocks only       
-     - "Atomic" properties only (Scalar, Strings, TimePoint)
-     - Metadata and FITSKeyword core elements
+    - Monolithic XISF files only
+        - XISF blocks with attachment block locations (neither inline nor embedded block locations as required 
+          for a complete baseline decoder)
+        - Planar pixel storage models, *however it assumes 2D images only* (with multiple channels)
+        - UInt8/16/32 and Float32/64 pixel sample formats
+        - Grayscale and RGB color spaces     
+    - Decoding:
+        - multiple Image core elements from a monolithic XISF file
+        - Support all standard compression codecs defined in this specification for decompression (zlib/lz4[hc]+
+          byte shuffling)
+    - Encoding:
+        - Single image core element
+        - Uncompressed data blocks only       
+    - "Atomic" properties only (Scalar, Strings, TimePoint)
+    - Metadata and FITSKeyword core elements
 
     What's not supported (at least by now):
-     - Read pixel data from XISF blocks with inline or embedded block locations
-     - Read pixel data in the normal pixel storage models
-     - Read pixel data in the planar pixel storage models other than 2D images
-     - Complex, Vector, Matrix and Table properties
-     - Any other not explicitly supported core elements (Resolution, Thumbnail, ICCProfile, etc.)
+    - Read pixel data from XISF blocks with inline or embedded block locations
+    - Read pixel data in the normal pixel storage models
+    - Read pixel data in the planar pixel storage models other than 2D images
+    - Complex, Vector, Matrix and Table properties
+    - Any other not explicitly supported core elements (Resolution, Thumbnail, ICCProfile, etc.)
 
     Usage example:
+    ```
     >>> from xisf import XISF
     >>> import matplotlib.pyplot as plt
     >>> xisf = XISF("file.xisf")
@@ -69,15 +70,17 @@ class XISF:
     >>> plt.imshow(im_data)
     >>> plt.show()
     >>> XISF.write("output.xisf", im_data, ims_meta[0], file_meta)
+    ```
 
     If the file is not huge and it contains only an image (or you're interested just in one of the 
     images inside the file), there is a convenience method for reading the data and the metadata:
+    ```
     >>> from xisf import XISF
     >>> import matplotlib.pyplot as plt    
     >>> im_data = XISF.read("file.xisf")
     >>> plt.imshow(im_data)
     >>> plt.show()
-
+    ```
 
     The XISF format specification is available at https://pixinsight.com/doc/docs/XISF-1.0-spec/XISF-1.0-spec.html
     """
